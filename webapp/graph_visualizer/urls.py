c0f92352e32b07274777eced210d10c429fdad72
urosh2003 <radukic.uros@gmail.com>
2025-09-23 20:34:45 +0200

    Implemented search and filtering

--- webapp/graph_visualizer/urls.py
+++ webapp/graph_visualizer/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('workspace/<str:workspace_id>/close/', views.close_workspace, name='close_workspace'),
     path('workspace/<str:workspace_id>/rename/', views.rename_workspace, name='rename_workspace'),
     path('workspace/<str:workspace_id>/switch/', views.switch_workspace, name='switch_workspace'),
+    path('workspace/add_search/', views.add_search, name='add_search'),
+    path('workspace/add_filter/', views.add_filter, name='add_filter'),
+    path('workspace/remove_search/', views.remove_search, name='remove_search'),
+    path('workspace/remove_filter/', views.remove_filter, name='remove_filter'),
 ]
